d755876157b382a0080b3f0fbe8fbe7feea6081e
Yixuan Sun <EaDen.Cen@gmail.com>
2026-09-24 14:59:44 -0700

    fix: scale artwork reference to available browser viewport

--- vite-project/src/control/HeroArtEditorDialog.tsx
+++ vite-project/src/control/HeroArtEditorDialog.tsx
@@ -244,7 +244,10 @@ export function HeroArtEditorDialog({
           </div>
           <div
             className="art-editor-reference"
-            style={{ aspectRatio: `${sourceSize.width} / ${sourceSize.height}` }}
+            style={{
+              aspectRatio: `${sourceSize.width} / ${sourceSize.height}`,
+              width: `min(100%, 980px, calc(46dvh * ${sourceSize.width / sourceSize.height}))`,
+            }}
           >
             <img
               src={fullSource}
